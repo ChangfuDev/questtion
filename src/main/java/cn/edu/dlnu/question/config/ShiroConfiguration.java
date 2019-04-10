365f22379a65fa1594f69e080ff65790908d335a
961099916 <961099916@qq.com>
2019-04-10 21:39:58 +0800

    基本完成 权限管理未完成

--- src/main/java/cn/edu/dlnu/question/config/ShiroConfiguration.java
+++ src/main/java/cn/edu/dlnu/question/config/ShiroConfiguration.java
@@ -26,21 +26,23 @@ public class ShiroConfiguration {
     shiroFilterFactoryBean.setSecurityManager(securityManager);
 //拦截器.
     Map<String, String> filterChainDefinitionMap = new LinkedHashMap<String, String>();
-
+    filterChainDefinitionMap.put("/admin/pages/**", "authc");
+    filterChainDefinitionMap.put("/admin/index.html", "authc");
+    filterChainDefinitionMap.put("/index.html", "authc");
+    filterChainDefinitionMap.put("/select.html", "authc");
+    filterChainDefinitionMap.put("/static/**", "anon");
     filterChainDefinitionMap.put("/**", "anon");
-
-
 //// 配置不会被拦截的链接 顺序判断
 //    filterChainDefinitionMap.put("/static/**", "anon");
 ////配置退出 过滤器,其中的具体的退出代码Shiro已经替我们实现了
-//    filterChainDefinitionMap.put("/logout", "logout");
+    filterChainDefinitionMap.put("/logout", "logout");
 ////<!-- 过滤链定义，从上向下顺序执行，一般将/**放在最为下边 -->:这是一个坑呢，一不小心代码就不好使了;
 ////<!-- authc:所有url都必须认证通过才可以访问; anon:所有url都都可以匿名访问-->
 //    filterChainDefinitionMap.put("/user/**", "authc");
 //// 如果不设置默认会自动寻找Web工程根目录下的"/login.jsp"页面
-    shiroFilterFactoryBean.setLoginUrl("/login");
+    shiroFilterFactoryBean.setLoginUrl("/pages/login.html");
 // 登录成功后要跳转的链接
-    shiroFilterFactoryBean.setSuccessUrl("/index");
+    shiroFilterFactoryBean.setSuccessUrl("/index.html");
 
 //未授权界面;
     shiroFilterFactoryBean.setUnauthorizedUrl("/403");
